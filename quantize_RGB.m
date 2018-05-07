e20c39f68c07a5c4531260038995fd129181d25f
Kiranpreet Kaur <35642755+kpkaur28@users.noreply.github.com>
2018-05-07 01:31:37 -0700

    Create  quantize_RGB.m

---
+++ quantize_RGB.m
@@ -0,0 +1,12 @@
+% Kiranpreet Kaur
+% Zaryab Farooq
+% ECS 174
+% 05/10/2018
+
+function [outputImg, meanColors] = quantize_RGB(origImg, k)
+img = imread("fish.jpeg");
+origImg = uint8(img);
+outputImg = uint8(img);
+k;
+meanColors;
+end 
